5a9b65d436bc943c7f96c8403adb705848dd9396
Joseph <joseph161216@gmail.com>
2023-02-15 22:21:25 -0800

    Add types and propTypes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewNote from "./components/NewNote";
 
+export type Note = {
+  id: string;
+} & NoteData; // type that adds on Notedata
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
 function App() {
   const [count, setCount] = useState(0);
 
--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,37 +1,72 @@
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import CreatableSelect from "react-select/creatable";
+import { NoteData, Tag } from "../App";
 
-function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+function NoteForm({ onSubmit }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectTags, setSelectTags] = useState<Tag[]>([]);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control ref={titleRef} required />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableSelect isMulti />
+              {/* convert what CreateableSelect expects to what are types are. label & value to label and id */}
+              <CreatableSelect
+                isMulti
+                value={selectTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+              />
             </Form.Group>
           </Col>
         </Row>
         <Row>
           <Form.Group controlId="markdown">
             <Form.Label>Body</Form.Label>
-            <Form.Control required as="textarea" rows={15} />
+            <Form.Control ref={markdownRef} required as="textarea" rows={15} />
           </Form.Group>
         </Row>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
             Save
           </Button>
-          <Button type="button" variant="outline-secondary">
-            Cancel
-          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
         </Stack>
       </Stack>
     </Form>
