cee2e03104aa35428d3e41162453e2ef78a07d6e
Joseph <joseph161216@gmail.com>
2023-02-15 21:14:01 -0800

    Setup and add routes

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} /> {/* /:id */}
+          <Route path="edit" element={<h1>Edit</h1>} /> {/* /:id/edit */}
+        </Route>
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
